9c89b6bc71253ccec1aa9fcc4dcc7268a74cb9e5
Saida Nurlanova <saidanurlanova01@gmail.com>
2025-05-04 14:56:49 +0500

    change access

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,10 +4,10 @@ import { authUser, checkIsAdmin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/", PostController.createPost);
+router.post("/", authUser, checkIsAdmin, PostController.createPost);
 router.get("/", PostController.getAllPosts);
 router.get("/:id", PostController.getPostById);
 router.patch("/:id", PostController.updatePost);
-router.delete("/:id", authUser,checkIsAdmin, PostController.deletePost);
+router.delete("/:id", authUser, checkIsAdmin, PostController.deletePost);
 
 export default router;
